ea63acb9e932c244c4bb16baa3e6521989b32325
Loris <loris@outlook.at>
2024-10-28 09:28:01 +0100

    streaming influence calculation

--- util.py
+++ util.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import os
 def get_epoch_checkpoints(model_dir):
-    c = sorted([int(str(x).split("-")[-1]) for x in Path(model_dir).glob("checkpoint-*")])
-    epoch_checkpoints = c[5::6]
-    if c[-1] not in epoch_checkpoints:
-        epoch_checkpoints.pop()
-        epoch_checkpoints.append(c[-1])
+    return sorted([int(str(x).split("-")[-1]) for x in Path(model_dir).glob("checkpoint-*")])
+    # epoch_checkpoints = c[5::6]
+    # if c[-1] not in epoch_checkpoints:
+    #     epoch_checkpoints.pop()
+    #     epoch_checkpoints.append(c[-1])
     return epoch_checkpoints
 
 def get_all_chunks(checkpoint_path,gradient_input_dir, gradients_per_file):
